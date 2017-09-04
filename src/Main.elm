cdad7ff8f3d936c40cdadd8bd71394d76facf9c0
David Mills <dave@1morepx.com>
2017-09-04 12:58:01 -0400

    Subscribe to animation frames

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,10 @@
 module Main exposing (main)
 
+import AnimationFrame
 import Html exposing (..)
 import Html.CssHelpers
 import MainStyles
+import Time
 
 
 -- PROGRAM
@@ -13,7 +15,7 @@ main =
     program
         { init = init
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         , view = view
         }
 
@@ -39,17 +41,34 @@ initialModel =
 
 
 
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    AnimationFrame.diffs Tick
+
+
+
 -- UPDATE
 
 
 type Msg
     = NoOp
+    | Tick Time.Time
 
 
 update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
     case msg of
         NoOp ->
+            ( model, Cmd.none )
+
+        Tick time ->
+            let
+                _ =
+                    Debug.log "Tick" time
+            in
             ( model, Cmd.none )
 
 
